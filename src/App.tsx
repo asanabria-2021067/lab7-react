a29ffc52865f63c26a578eca87331b5a31dfab71
Angel Sanabria <as1945228@gmail.com>
2026-05-19 19:26:36 -0600

    feat: add App and main entry files for calculator application

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Calculator } from './components/Calculator'
+import './index.css'
+
+export const App: React.FC = () => {
+  return (
+    <main className="container">
+      <Calculator />
+    </main>
+  )
+}
+export default App
